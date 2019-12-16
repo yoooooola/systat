f1caaafb93fdbea26dac77cb2401b86c6671b00f
jaySHKorea <seung2586@gmail.com>
2019-12-16 21:24:54 +0900

    add -m option

--- memoryUse.c
+++ memoryUse.c
@@ -2,7 +2,7 @@
 // 커맨드 입력 시 전체 메모리의 사용량 관찰 가능
 // 커맨드 입력 시 메모리 사용량을 관찰하고 싶은 프로그램을 함께 입력할 수 있다 (option)
 // 입력받은 프로그램의 PID를 찾아 /proc/PID/meminfo 에 접근 및 해당 프로그램의 메모리 사용량 관찰
-// 만약 조금 더 자세한 정보를 요구할 경우, top 커맨드를 구현 혹은 파이프를 통해 활용할 예정
+// -m (option) : 메모리 사용량 정보를 mb로 표시한다 (기본옵션 : kb)
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,19 +22,11 @@ struct meminfo {
 }; // structure saves meminfo variables
 
 struct calinfo {
-<<<<<<< HEAD
-	int used;
-//	int bufCache;
-	int swapUsed;
-	double nomMem; // nomial memory usage ratio
-	double actMem; // actual memory usage ratio
-=======
     int used;
     //      int bufCache;
     int swapUsed;
     double nomMem; // nomial memory usage ratio
     double actMem; // actual memory usage ratio
->>>>>>> 2160f342e0f4ba72d87e315232ce79cf3583ea54
 }; // structure saves calculating result using meminfo
 
 int checkM = 0;
@@ -47,80 +39,6 @@ void reset(); // reset all text conf
 void printMemory(struct meminfo, struct calinfo, char); // print memory info to screen
 
 int main(int ac, char *av[]) {
-<<<<<<< HEAD
-
-
-	if(ac > 1) {
-		while(--ac) {
-			if(av[ac][0] == '-') { // check option
-				if(av[ac][1] == 'm') checkM = 1; // option present mb
-			}
-		}
-	}
-
-	struct meminfo m;
-	struct calinfo c;
-
-	// get memory info
-	getMemory(&m);
-
-	// caculate using memory info
-	calMemory(m, &c);
-
-	// print memory info
-	if(checkM == 1) printMemory(m, c, 'm');
-	else printMemory(m, c, 'k'); // basic option is kilobyte
-
-	return 0;
-}
-
-void printMemory(struct meminfo m, struct calinfo c, char option) {
-
-	red();
-	printf("\nMemory ↓\n\n");
-
-	reset();
-	
-	if(option == 'k') { // present with kilobyte
-		printf("Total: %d \t Used: %d\n", m.memTotal, c.used);
-		printf("Free: %d \t Available: %d\n\n", m.memFree, m.memAvailable);
-		printf("Shared Memory: %d\n", m.sharedMem);
-		printf("Buffer: %d\n", m.buffers);
-		printf("Cache: %d\n\n", m.cached);
-	}
-
-	else if(option == 'm') { // megabyte
-        	printf("Total: %d \t Used: %d\n", (m.memTotal/BUFSIZE), (c.used/BUFSIZE));
-                printf("Free: %d \t Available: %d\n\n", (m.memFree/BUFSIZE), (m.memAvailable/BUFSIZE));
-                printf("Shared Memory: %d\n", (m.sharedMem/BUFSIZE));
-                printf("Buffer: %d\n", (m.buffers/BUFSIZE));
-                printf("Cache: %d\n\n", (m.cached/BUFSIZE));
-	}
-
-	red();
-        printf("Swap ↓\n\n");
-
-	reset();
-	
-	if(option == 'k')
-		printf("Total: %d\n Used: %d\n Free: %d\n\n", m.swapTotal, c.swapUsed, m.swapFree);
-	else if(option == 'm')
-		printf("Total: %d\n Used: %d\n Free: %d\n\n", (m.swapTotal/BUFSIZE), (c.swapUsed/BUFSIZE),(m.swapFree/BUFSIZE));
-
-	blue();
-	printf("Nomial Memory Usage : ");
-
-	reset();
-	printf("%.2f%%\n", c.nomMem);
-	
-	blue();
-	printf("Actual Memory Usage : ");
-	
-	reset();
-	printf(" %.2f%%\n\n", c.actMem);
-
-	return;	
-=======
     
     
     if(ac > 1) {
@@ -193,7 +111,6 @@ void printMemory(struct meminfo m, struct calinfo c, char option) {
     printf(" %.2f%%\n\n", c.actMem);
     
     return;
->>>>>>> 2160f342e0f4ba72d87e315232ce79cf3583ea54
 }
 
 void red() {
@@ -209,23 +126,6 @@ void reset() {
 }
 
 void calMemory(struct meminfo m, struct calinfo *c) {
-<<<<<<< HEAD
-	
-	// used = total - free
-	c->used = (m.memTotal) - (m.memFree);
-//	c->bufCache = (m.cached) + (m.buffers);
-	c->swapUsed = (m.swapTotal) - (m.swapFree);
-	
-	// caculate nomial memory usage ratio
-	c->nomMem = (double)(c->used) / (double)(m.memTotal);
-	c->nomMem = (c->nomMem) * 100; // for presenting by percentage
-	
-	// caculate actual memory usage ratio
-	c->actMem = (double)((m.memTotal) - (m.memAvailable)) / (double)(m.memTotal);
-	c->actMem = (c->actMem) * 100;
-
-	return;
-=======
     
     // used = total - free
     c->used = (m.memTotal) - (m.memFree);
@@ -241,7 +141,6 @@ void calMemory(struct meminfo m, struct calinfo *c) {
     c->actMem = (c->actMem) * 100;
     
     return;
->>>>>>> 2160f342e0f4ba72d87e315232ce79cf3583ea54
 }
 
 void getMemory(struct meminfo* m) {
@@ -284,3 +183,4 @@ void getMemory(struct meminfo* m) {
     
     return;
 }
+
